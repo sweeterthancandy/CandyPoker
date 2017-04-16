f000c647cb6c3ebc47d1a46161d53efd399e1024
gerry candy <g3candy@gmail.com>
2017-04-16 20:19:04 +0100

    made some 5 card eval improvements

--- ps/core/eval.h
+++ ps/core/eval.h
@@ -1,10 +1,14 @@
 #ifndef PS_EVAL_H
 #define PS_EVAL_H
 
+#include <array>
+
 #include <boost/range/algorithm.hpp>
 
 #include "card_traits.h"
 #include "generate.h"
+
+#include "ps/core/cards.h"
 
 namespace ps{
 
@@ -12,20 +16,15 @@ namespace detail{
         struct eval_impl{
                 eval_impl():order_{1}{
                         m_.resize( 37 * 37 * 37 * 37 * 31 * 2 +1 );
+
+                        std::array<int,4> suit_map{ 2,3,5,7 };
+                        for( size_t i{0};i!=52;++i){
+                                flush_device_[i] = suit_map[card_decl::get(i).suit().id()];
+                                rank_device_[i] = card_decl::get(i).rank().id();
+                        }
                 }
                 void next( bool f, long a, long b, long c, long d, long e){
                         std::uint32_t m = map(f,a,b,c,d,e);
-                        #if 0
-                        std::cout << ( f ? "f " : "  " )
-                                << traits_.rank_to_string(a)
-                                << traits_.rank_to_string(b) 
-                                << traits_.rank_to_string(c) 
-                                << traits_.rank_to_string(d) 
-                                << traits_.rank_to_string(e)
-                                << "  ~  " << m
-                                << "  => " << order_
-                                << std::endl;
-                                #endif
                         assert( m_[m] == 0 && "not injective");
                         m_[m] = order_;
                         ++order_;
@@ -35,31 +34,30 @@ namespace detail{
                 }
                 void end(){}
                 std::uint32_t eval(long a, long b, long c, long d, long e)const{
-                        bool f{traits_.suit(a) == traits_.suit(b) &&
-                            traits_.suit(a) == traits_.suit(c) &&
-                            traits_.suit(a) == traits_.suit(d) &&
-                            traits_.suit(a) == traits_.suit(e)};
-                        std::uint32_t m = map(f,
-                                              traits_.rank(a),traits_.rank(b),
-                                              traits_.rank(c),traits_.rank(d),
-                                              traits_.rank(e));
-                        //PRINT(m);
-                        if( m_[m] == 0 ){
-                                std::cout << ( f ? "f " : "  " )
-                                        << traits_.to_string(a)
-                                        << traits_.to_string(b) 
-                                        << traits_.to_string(c) 
-                                        << traits_.to_string(d) 
-                                        << traits_.to_string(e)
-                                        << "  ~  " << m
-                                        << "\n";
-                        }
+                        auto f_aux{ flush_device_[a] * 
+                                    flush_device_[b] *
+                                    flush_device_[c] *
+                                    flush_device_[d] *
+                                    flush_device_[e] };
 
+                        bool f{false};
+                        switch(f_aux){
+                        case 2*2*2*2*2:
+                        case 3*3*3*3*3:
+                        case 5*5*5*5*5:
+                        case 7*7*7*7*7:
+                                f = true;
+                                break;
+                        }
+                        std::uint32_t m = map(f,
+                                              rank_device_[a],rank_device_[b],
+                                              rank_device_[c],rank_device_[d],
+                                              rank_device_[e]);
                         assert( m_[m] && "unmapped value");
                         return m_[m];
                 }
                 std::uint32_t map(bool f, long a, long b, long c, long d, long e)const{
-                        //                                   2 3 4 5  6  7  8  9  T  J  Q  K  A
+                        //                                 2 3 4 5  6  7  8  9  T  J  Q  K  A
                         std::array<std::uint32_t, 13> p = {2,3,5,7,11,13,17,19,23,27,29,31,37};
                         return ( f ? 2 : 1 ) * 
                                 p[a] * p[b] * 
@@ -70,6 +68,9 @@ namespace detail{
                 std::string name_;
                 std::vector<std::uint32_t> m_;
                 card_traits traits_;
+
+                std::array<int, 52> flush_device_;
+                std::array<int, 52> rank_device_;
         };
 }
 
@@ -82,14 +83,6 @@ struct eval{
                 return this->operator()(cards[0], cards[1], cards[2], cards[3], cards[4] );
         }
         std::uint32_t operator()(long a, long b, long c, long d, long e)const{
-                #if 0
-                std::cout
-                        << traits_.rank_to_string(a)
-                        << traits_.rank_to_string(b) 
-                        << traits_.rank_to_string(c) 
-                        << traits_.rank_to_string(d) 
-                        << traits_.rank_to_string(e) << "\n";
-                #endif
                 return impl_.eval(a,b,c,d,e);
         }
         std::uint32_t operator()(long a, long b, long c, long d, long e, long f)const{

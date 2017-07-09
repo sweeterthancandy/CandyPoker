ed4e82e104ff8d4f8efe46499b6dcdbc77c0f9aa
dest <g3candy@gmail.com>
2017-07-09 22:00:52 +0100

    eod (working on parser)

--- ps.cpp
+++ ps.cpp
@@ -1,60 +1,234 @@
-#include "ps/calculator_detail.h"
-#include "ps/calculator.h"
-#include "ps/heads_up.h"
-#include "ps/frontend.h"
-#include "ps/algorithm.h"
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <locale>
+#include <codecvt>
+#include <experimental/filesystem>
 
-#include <mutex>
-#include <thread>
-#include <condition_variable>
+#include <boost/optional.hpp>
+#include <boost/variant.hpp>
 
-#include <boost/timer/timer.hpp>
-#include <boost/mpl/size_t.hpp>
+#include "ps/detail/print.h"
+        
+namespace fs = std::experimental::filesystem;
 
-#include "ps/detail/visit_sequence.h"
-#include "ps/detail/visit_combinations.h"
+/*
 
-using namespace ps;
+   Need to be able to detect the kind of hands they are, ie find
+   all finds which where are preflop allin/fold situtation
 
-struct holdem_hand_caster{
-        template<class T>
-        std::string operator()(T const& val)const{
-                return holdem_hand_decl::get(val).to_string();
-        }
+*/
+
+struct player_decl{
+        void decl_name(std::string const& name){ return name_; }
+        void decl_stack(double stack){ return stack_; }
+private:
+        boost::optional<std::string> name_;
+        double stack_;
 };
 
-template<class Con>
-auto to_string(Con const& con){
-        return detail::to_string(con, holdem_hand_caster{} );
+struct game_decl{
+        void decl_sb(double sb){ sb_ = sb; }
+        void decl_bb(double bb){ bb_ = bb; }
+        void decl_player(player_decl const& p){ players_.push_back(p); }
+private:
+        boost::optional<double> sb_;
+        boost::optional<double> bb_;
+        std::vector< player_decl > players_;
+};
+
+using player_id = int;
+
+/*
+ *  From past experince I want this
+ */
+enum SubParserCtrl{
+        SubParserCtrl_Parsed,
+        SubParserCtrl_Failed
+};
+struct subparser
+{
+        using line_iterator = std::vector<std::string>::const_iterator;
+
+        virtual ~subparser()=default;
+
+        virtual SubParserCtrl execute( parser_context& ctx)=0;
+};
+
+struct subparser_or{
+        SubParserCtrl execute( parser_context& ctx)override{
+                for(auto child : children_){
+                        switch(child->execute( ctx )){
+                        case SubParserCtrl_Parsed:
+                                return SubParserCtrl_Parsed;
+                        default:
+                                break;
+                        }
+                }
+                return SubParserCtrl_Failed;
+        }
+private:
+        std::vector<subparser*> children_;
+};
+
+
+
+namespace pokerstars{
+        
+        // by domain
+        namespace header{
+
+                struct tournament{
+                        SubParserCtrl execute( parser_context& ctx, factory& fac)override{
+                                std::smatch m;
+                                if( std::regex_searh(ctx.current_line(), m, rgx_ ) ){
+                                        fac.add( 
+                                }
+                        }
+                private:
+                        std::regex rgx_{R"(^PokerStars Hand #(\d+): Tournament #(\d+),)"};
+                        //std::regex rgx_{R"(PokerStars Hand #(\d+): Tournament #(\d+), $14.10+$0.90 USD Hold'em No Limit - Level I (10/20) - 2017/07/02 14:36:52 WET [2017/07/02 9:36:52 ET])"};
+                };
+        }
 }
 
+
+
+struct parser_global_factory{
+
+        template<class T>
+        void register_subparser(std::string const& name,
+                                std::string const& domain){
+                auto maker = [](){ return std::make_shared<T>(); };
+                maker_.insert(std::make_pair(name, maker));
+                dmaker_[domain] = maker;
+        }
+
+        parser_global_factory* get(){
+                static parser_global_factory* mem = 0;
+                if( ! mem )
+                        mem = new parser_global_factory;
+                return mem;
+        }
+private:
+        std::map<std::string, std::function<std::shared_ptr<subparser>()> > maker_;
+        std::map<std::string, std::vector<std::function<std::shared_ptr<subparser>()> > > maker_;
+};
+
+struct action_post_sb{
+        action_post_sb(player_id player):
+                player_(player)
+        {}
+        void execute( factory& fac){
+                fac.post_sb();
+        }
+private:
+        player_id id_;
+};
+
+
+struct parser_item{
+        std::regex rgx;
+};
+struct parser_namespace{
+        std::vector<parser_item> item_;
+};
+struct parser_factory{
+};
+struct parser_context{
+};
+
+struct parser{
+        parser(){
+                factory fac;
+                auto root   = new subparser_or{};
+                
+                auto header = new subparser_or{};
+                header->add( new ps_tourn_header{} );
+                
+                auto info   = new subparser_header;
+                info->dd(new ps_tourn_
+
+
+
+        }
+
+        void section_parser_(std::vector<std::string>&& lines){
+                
+                for( auto const& l : lines)
+                        std::cout << l << "\n";
+                std::cout << "END_SECTION\n";
+        }
+        void parse(std::istream& istr){
+                std::vector<std::string> lines;
+
+                auto produce = [&](){
+                        section_parser_(std::move(lines));
+                        lines.clear();
+                };
+
+
+                for(;;){
+                        std::string raw_line;
+                        std::getline(istr, raw_line);
+                        if( istr.eof() && raw_line.empty())
+                                break;
+
+                        if( istr.eof())
+                                break;
+
+                        if( std::regex_match(raw_line, whitespace_rgx_ ) ){
+                                if( lines.size())
+                                       produce();
+                        } else {
+                                lines.push_back(std::move(raw_line));
+                        }
+                }
+
+                if( lines.size() ){
+                        produce();
+                }
+        }
+private:
+        std::regex whitespace_rgx_{R"(\s+)"};
+        parser_context pc_proto_;
+};
+
+
 int main(){
-        boost::timer::auto_cpu_timer at;
-        std::map<std::vector<ps::holdem_id>, int> world; 
-        size_t sigma{0};
-        detail::visit_exclusive_combinations<3>(
-                [&](auto... args)
-                {
-                        std::vector<ps::holdem_id> from{ static_cast<ps::holdem_id>(args)...};
-                        auto t{ permutate_for_the_better(
-                                        std::vector<ps::holdem_id>{
-                                                static_cast<ps::holdem_id>(args)...} ) };
-                        auto to{std::get<1>(t)};
-                        #if 0
-                        std::cout 
-                                << to_string(from) << " => " 
-                                << to_string(to) << " " 
-                                << detail::to_string(std::get<0>(t)) << "\n";
-                                #endif
 
-                        ++world[std::get<1>(t)];
-                        ++sigma;
 
-                },
-                detail::true_,
-                holdem_hand_decl::max_id-1
-        );
-
-        PRINT_SEQ((sigma)(world.size()));
+        std::regex HH_rgx("^HH");
+        std::stack<fs::path> stack;
+        std::vector<std::string> to_process;
+        stack.emplace("hh/2017");
+        for( ;stack.size(); ){
+                auto item{ stack.top() };
+                stack.pop();
+                for(fs::directory_iterator di(item),de;di!=de;++di){
+                        switch(di->status().type()){
+                        case fs::file_type::regular:
+                                if( std::regex_search( di->path().filename().string(), HH_rgx ) ){
+                                        to_process.push_back( di->path().string() );
+                                }
+                                break;
+                        case fs::file_type::directory:
+                                stack.push(di->path());
+                                break;
+                        }
+                }
+        }
+        parser p;
+        int i=0;
+        for( auto const& f : to_process ){
+                PRINT(f);
+                std::ifstream ifstr(f, std::ifstream::binary);
+                if( ! ifstr.is_open()){
+                        std::cerr << "unable to open " << f << "\n";
+                        continue;
+                }
+                p.parse(ifstr);
+        }
+        
 
 }
---
+++ ps/calculator_result.h
@@ -0,0 +1,85 @@
+#ifndef PS_CALCULATOR_RESULT_H
+#define PS_CALCULATOR_RESULT_H
+
+namespace ps{
+
+template<size_t N>
+struct detailed_result_type{
+        detailed_result_type(detailed_result_type const&)=default;
+        detailed_result_type():
+                sigma_{0}
+        {
+                std::memset( data_.begin(), 0, sizeof(data_));
+        }
+        auto& data_access(size_t i, size_t j){
+                return data_[i * N + j];
+        }
+        auto data_access(size_t i, size_t j)const{
+                return data_[i * N + j];
+        }
+        size_t const* data()const{ return reinterpret_cast<size_t const*>(data_.begin()); }
+
+        template<class Archive>
+        void serialize(Archive& ar, unsigned int){
+                ar & sigma_;
+                ar & data_;
+        }
+
+        auto sigma()const{ return sigma_; }
+        auto& sigma(){ return sigma_; }
+        
+private:
+        size_t sigma_;
+
+        // access float, so can have a static view for all 
+        //  2,3,...9 etc without injeritace (see view_t)
+        std::array<
+                        size_t,
+                N * N
+        > data_;
+};
+
+
+template<size_t N>
+struct detailed_observer_type{
+        template<class Int, class Vec>
+        void operator()(Int a, Int b, Int c, Int d, Int e, Vec const& ranked){
+                /*
+                        Here I need a quick way to work out the lowest rank,
+                        as well as how many are of that rank, and I need to
+                        find them. I think this is the quickest
+                */
+                auto lowest{ ranked[0] };
+                size_t count{1};
+                for(size_t i=1;i<ranked.size();++i){
+                        if( ranked[i] == lowest ){
+                                ++count;
+                        } else if( ranked[i] < lowest ){
+                                lowest = ranked[i]; 
+                                count = 1;
+                        }
+                }
+                for(size_t i=0;i!=ranked.size();++i){
+                        if( ranked[i] == lowest ){
+                                ++result.data_access(i,count-1);
+                        }
+                }
+                ++result.sigma();
+        }  
+        template<class View_Type>
+        void append(View_Type const& view){
+                result.sigma() += view.sigma();
+                for(size_t i=0;i!=N;++i){
+                        for(size_t j=0;j!=N;++j){
+                                result.data_access(i, j) += view.player(i).nwin(j);
+                        }
+                }
+        }
+        auto make(){ return result; }
+private:
+        detailed_result_type<N> result;
+};
+
+} // ps
+
+#endif // PS_CALCULATOR_RESULT_H

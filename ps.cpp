550446ec85a081fa12d703b635c211cbde2f594d
dest <g3candy@gmail.com>
2017-08-12 20:07:14 +0100

    added new processor framework

--- ps.cpp
+++ ps.cpp
@@ -67,7 +67,7 @@ private:
 };
 #endif
 
-#if 0
+#if 1
 int main(){
         support::processor proc;
         for( size_t i=0; i!= std::thread::hardware_concurrency();++i){
@@ -104,290 +104,8 @@ int main(){
 }
 #endif
 
-struct processor{
 
-        using work_t = std::function<void()>;
-
-        enum Ctrl{
-                Ctrl_Success,                    // When there is something to schedule
-                Ctrl_NothingLeftButNotFinished,  // When everything is scheduled, but not finished
-                Ctrl_NotReady,                   // When we're waiting for a task to finish first
-                Ctrl_Finished                    // When everything has been scheduled and finished     
-        };
-
-        struct schedulable : std::enable_shared_from_this<schedulable>{
-                virtual std::tuple<Ctrl, boost::optional<work_t> > select()=0;
-        };
-
-        struct single_task : schedulable{
-                enum State{
-                        State_ReadyToRun,
-                        State_Selected,
-                        State_Finished
-                };
-                explicit single_task(work_t w)
-                        :w_{std::move(w)}
-                {
-                }
-                std::tuple<Ctrl, boost::optional<work_t> > select()override{
-                        switch(state_){
-                        case State_ReadyToRun:
-                                state_ = State_Selected;
-                                return std::make_tuple(
-                                        Ctrl_Success,
-                                        work_t{[_w=std::move(w_),handle=shared_from_this(),this](){
-                                                _w();
-                                                state_ = State_Finished;
-                                        }}
-                                );
-                        case State_Selected:
-                                return std::make_tuple(
-                                        Ctrl_NothingLeftButNotFinished,
-                                        boost::none
-                                );
-                        case State_Finished:
-                                return std::make_tuple(
-                                        Ctrl_Finished,
-                                        boost::none
-                                );
-                        }
-                        __builtin_unreachable();
-                }
-        private:
-                std::atomic_int state_{State_ReadyToRun};
-                work_t w_;
-        };
-
-
-        struct unsequenced_group : schedulable{
-                // Returns
-                //      
-                //      Ctrl_Success                    => one child has something to select()
-                //      Ctrl_NotReady                   => no child had something to select(), 
-                //                                         but one or more have something 
-                //                                         soon
-                //      Ctrl_NothingLeftButNotFinished  => have nothing left to select(), but
-                //                                         some children are still running
-                //      Ctrl_Finished                   => every child has finished
-                //
-                void push(std::shared_ptr<schedulable> ptr){
-                        vec_.push_back(std::move(ptr));
-                }
-
-                std::tuple<Ctrl, boost::optional<work_t> > select()override{
-                        // They get selected in order ie
-                        //     {p0_Start, p1_start, p2_start,...}
-                        
-                        Ctrl default_reason = Ctrl_Finished;
-                        for( auto& ptr : vec_){
-                                // There might be something to schedule,
-                                // but it might not be ready
-                                auto ret = ptr->select();
-                                switch(std::get<0>(ret)){
-                                case Ctrl_Success:
-                                        ++selected_;
-                                        return std::make_tuple(
-                                                Ctrl_Success,
-                                                work_t{[_w=std::get<1>(ret).get(),handle=shared_from_this(),this](){
-                                                        _w();
-                                                        ++done_;
-                                                }}
-                                        );
-                                case Ctrl_NotReady:
-                                        default_reason = Ctrl_NotReady;
-                                        continue;
-                                case Ctrl_NothingLeftButNotFinished:
-                                        // If there's already a reason, then the current
-                                        // reason is better
-                                        if( default_reason == Ctrl_Finished )
-                                                default_reason = Ctrl_NothingLeftButNotFinished;
-                                        continue;
-                                case Ctrl_Finished:
-                                        continue;
-                                }
-                                __builtin_unreachable();
-                        }
-                        return std::make_tuple(
-                                default_reason,
-                                boost::none
-                        );
-                }
-        private:
-                std::atomic_int selected_{0};
-                std::atomic_int done_{0};
-                std::vector<std::shared_ptr<schedulable> > vec_;
-        }; 
-        
-        struct sequenced_group : schedulable{
-                void push(std::shared_ptr<schedulable> ptr){
-                        vec_.push_back(std::move(ptr));
-                }
-                std::tuple<Ctrl, boost::optional<work_t> > select()override{
-                        // They get selected in order ie
-                        //     {p0_Start, p0_end, p1_start, p1_end,...}
-                        
-                        for(;iter_ < vec_.size();){
-
-                                auto ret = vec_[iter_]->select();
-                                switch(std::get<0>(ret)){
-                                case Ctrl_Success:
-                                        ++selected_;
-                                        return std::make_tuple(
-                                                Ctrl_Success,
-                                                work_t{[_w=std::get<1>(ret).get(),handle=shared_from_this(),this](){
-                                                        _w();
-                                                        ++done_;
-                                                }}
-                                        );
-                                case Ctrl_NotReady:
-                                        return std::move(ret);
-                                case Ctrl_NothingLeftButNotFinished:
-                                        return std::make_tuple(
-                                                ( iter_ + 1 == vec_.size() ?
-                                                        Ctrl_NothingLeftButNotFinished :
-                                                        Ctrl_NotReady ),
-                                                boost::none
-                                        );
-                                case Ctrl_Finished:
-                                        ++iter_;
-                                        continue;
-                                }
-                                __builtin_unreachable();
-                        }
-                        // We only get here by a sequence of zero or
-                        // more Ctrl_Finished
-                        return std::make_tuple(
-                                Ctrl_Finished,
-                                boost::none
-                        );
-                }
-        private:
-                size_t iter_{0};
-                std::atomic_int selected_{0};
-                std::atomic_int done_{0};
-                std::vector<std::shared_ptr<schedulable> > vec_;
-        }; 
-
-        // This is just for creationable purposes,
-        // because we only every create process_groups in
-        // the form
-        //
-        //      auto pg0 = std::make_shared<process_group>();
-        //      pg0->add(f);
-        //      pg0->add(g);
-        //      pg0->sequence_point();
-        //      pg0->add(h);
-        //      auto pg1 = std::make_shared<process_group>();
-        //      pg1->add(pg1);
-        //      pg1->add(i);
-        //      pg1->sequence_point();
-        //      pg1->add(j);
-        //
-        //      proc.accept(pg1);
-        //      
-        struct process_group{
-                process_group()
-                        :group_{std::make_shared<sequenced_group>()}
-                {}
-
-                /////////////////////////////////////////////////////////
-                //
-                //           Creational interface
-                //
-                /////////////////////////////////////////////////////////
-                void sequence_point(){
-                        if( working_stack_.empty())
-                                return;
-                        auto g = std::make_shared<unsequenced_group>();
-                        for( auto& ptr : working_stack_ ){
-                                g->push( std::move(ptr) );
-                        }
-                        group_->push(std::move(g));
-                        working_stack_.clear();
-                }
-                void push(work_t w){
-                        working_stack_.push_back(
-                                std::make_shared<single_task>(std::move(w))
-                        );
-                }
-                auto compile(){
-                        this->sequence_point();
-                        return std::move(group_);
-                }
-                void push(std::unique_ptr<process_group> pg){
-                        working_stack_.push_back(
-                                pg->compile()
-                        );
-                }
-        private:
-                std::shared_ptr<sequenced_group> group_;
-                std::vector< std::shared_ptr<schedulable> > working_stack_;
-        };
-
-        processor()
-                :head_{std::make_shared<unsequenced_group>()}
-        {
-        }
-        ~processor(){
-                join(); // maybe
-        }
-        void join(){
-                //std::cout << "join()\n";
-                running_ = false;
-                no_work_barrier_.notify_all();
-                if( threads_.size() ){
-                        for( auto& t : threads_ )
-                                t.join();
-                        threads_.clear();
-                }
-        }
-        void accept( std::unique_ptr<process_group> group ){
-                //std::cout << "accept()\n";
-                std::unique_lock<std::mutex> lock(mtx_);
-                head_->push(group->compile());
-                no_work_barrier_.notify_all();
-        }
-        void spawn(){
-                //std::cout << "spawn()\n";
-                threads_.emplace_back(
-                        [this]()mutable{
-                        __main__();
-                });
-        }
-private:
-        void __main__(){
-                //std::cout << "__main__()\n";
-                for(;;){
-                        decltype(this->head_->select()) ret;
-                        {
-                                //std::cout << "locking()\n";
-                                std::unique_lock<std::mutex> lock(mtx_);
-                                ret = head_->select();
-                                switch(std::get<0>(ret)){
-                                case Ctrl_Success:
-                                        break;
-                                case Ctrl_NotReady:
-                                case Ctrl_NothingLeftButNotFinished:
-                                case Ctrl_Finished:
-                                        if( ! running_ )
-                                                return;
-                                        no_work_barrier_.wait(lock);
-                                        continue;
-                                }
-                        }
-                        std::get<1>(ret).get()();
-                        std::unique_lock<std::mutex> lock(mtx_);
-                        no_work_barrier_.notify_all();
-                }
-        }
-        std::mutex mtx_;
-        std::condition_variable no_work_barrier_;
-        std::thread schedular_thread_;
-        std::vector<std::thread> threads_;
-        std::atomic_bool running_{true};
-        std::shared_ptr<unsequenced_group> head_;
-};
-
+#if 0
 
 auto make_proto(std::string const& tok){
         auto a = std::make_unique<processor::process_group>();
@@ -406,7 +124,7 @@ auto make_proto(std::string const& tok){
 }
 
 
-int main(){
+int processor_test0(){
         processor proc;
         #if 1
         for(int i=0;i!=100;++i)
@@ -421,13 +139,14 @@ int main(){
         g->push( make_proto("c"));
         auto h = std::make_unique<processor::process_group>();
         h->push(std::move(g));
-        h->sequence_point();
+        //h->sequence_point();
         h->push([]{std::cout << "Finished\n"; });
 
-        proc.accept(std::move(g));
+        proc.accept(std::move(h));
         #endif
         proc.join();
 }
+#endif 
 
 
 

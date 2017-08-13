202e2efbe200e1718e8bebba8450fdc0504e964f
dest <g3candy@gmail.com>
2017-08-13 10:13:06 +0100

    worse version

--- ps.cpp
+++ ps.cpp
@@ -14,63 +14,68 @@
 
 using namespace ps;
 
+using shared_fut_t = std::shared_future<
+        std::shared_ptr<equity_breakdown>
+>;
+using eval_t = std::function<
+        std::shared_ptr<equity_breakdown>()
+>;
 
 struct equity_future{
-        using result_t = std::shared_future<
-                std::shared_ptr<equity_breakdown>
-        >;
         equity_future()
                 : impl_{ &equity_evaluator_factory::get("principal") }
         {}
-        result_t schedual_group(support::processor::process_group& pg, holdem_hand_vector const& players){
+        eval_t schedual_group(support::processor::process_group& pg, holdem_hand_vector const& players){
                 std::cout << "schedual_group(" << players << "\n";
 
                 if( players.is_standard_form() ){
-                        auto iter = m_.find(players);
-                        if( iter != m_.end() ){
-                                return iter->second;
-                        }
-                        auto task = std::make_shared<std::packaged_task<std::shared_ptr<equity_breakdown>()> >(
-                                [players,this](){
-                                return impl_->evaluate(players);
-                        });
-                        m_.emplace(players, std::move(task->get_future()));
-
-                        // yuk
-                        auto w = [task]()mutable{
-                                (*task)();
+                        auto sf = do_schedual_standard_form_(pg, players);
+                        return eval_t{
+                                [_sf=std::move(sf)]()
+                                { return _sf.get(); }
                         };
-                        pg.push(std::move(w));
-                        return schedual_group(pg, players);
                 } else {
                         auto p =  permutate_for_the_better(players) ;
                         auto& perm = std::get<0>(p);
                         auto const& perm_players = std::get<1>(p);
 
-                        auto perm_ret = schedual_group(pg, perm_players);
-                        auto task = std::make_shared<std::packaged_task<std::shared_ptr<equity_breakdown>()> >(
-                                [perm,perm_ret](){
-                                return 
-                                        std::make_shared<equity_breakdown_permutation_view>(
-                                                perm_ret.get(),
-                                                perm
+                        auto sf = do_schedual_standard_form_(pg, perm_players);
+                        return eval_t(
+                                [_perm=std::move(perm),_sf=std::move(sf)]()
+                                { 
+                                        return std::make_shared<equity_breakdown_permutation_view>(
+                                                _sf.get(),
+                                                _perm
                                         );
-                        });
-                        result_t ret = task->get_future();
-                        // yuk
-                        auto w = [task]()mutable{
-                                (*task)();
-                        };
-                        pg.push(std::move(w));
-                        return std::move(ret);
+                                }
+                        );
                 }
-
-
         }
 private:
+        shared_fut_t do_schedual_standard_form_(support::processor::process_group& pg, holdem_hand_vector const& players){
+                std::cout << "do_schedual_standard_form_(" << players << "\n";
+
+                auto iter = m_.find(players);
+                if( iter != m_.end() ){
+                        return iter->second;
+                }
+                auto task = std::make_shared<std::packaged_task<std::shared_ptr<equity_breakdown>()> >(
+                        [players,this](){
+                        return impl_->evaluate(players);
+                });
+                m_.emplace(players, std::move(task->get_future()));
+
+                // yuk
+                auto w = [task]()mutable{
+                        (*task)();
+                };
+                pg.push(std::move(w));
+                return do_schedual_standard_form_(pg, players);
+        }
+
         equity_evaluator* impl_;
         // only cache those in standard form
-        std::map< holdem_hand_vector, result_t > m_;
+        std::map< holdem_hand_vector, shared_fut_t > m_;
 };
 
 struct class_equity_future{
@@ -83,35 +88,26 @@ struct class_equity_future{
         result_t schedual_group(support::processor::process_group& pg, holdem_class_vector const& players){
                 std::cout << "schedual_group(" << players << "\n";
                 
-                std::vector< std::tuple< std::vector<int>, result_t > > items;
+                std::vector< eval_t > items;
 
                 for( auto hv : players.get_hand_vectors()){
-                        auto p =  permutate_for_the_better(hv) ;
-                        auto& perm = std::get<0>(p);
-                        auto const& perm_players = std::get<1>(p);
-                        auto fut = ef_.schedual_group(pg, perm_players);
-                        items.emplace_back(perm, fut);
+                        items.emplace_back( ef_.schedual_group(pg, hv ) );
                 }
                 auto task = std::make_shared<std::packaged_task<std::shared_ptr<equity_breakdown>()> >(
                         [n_=players.size(),items_=std::move(items)](){
                         auto result = std::make_shared<equity_breakdown_matrix_aggregator>(n_);
-                        for( auto& t : items_ ){
-                                result->append(
-                                        *std::make_shared<equity_breakdown_permutation_view>(
-                                                std::get<1>(t).get(),
-                                                std::get<0>(t)));
+                        for( auto const& item : items_ ){
+                                result->append( *item() );
                         }
                         return result;
                 });
                 result_t fut = task->get_future();
-                m_.emplace(players, fut);
                 pg.sequence_point();
                 pg.push([task]()mutable{ (*task)(); });
                 return fut;
         }
 private:
         ::equity_future ef_;
-        std::map< holdem_hand_vector, result_t > m_;
 };
 
 struct class_range_equity_future{

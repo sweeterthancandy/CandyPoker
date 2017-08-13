a67d30aed9dd7a6138f9b7f4c3af381ef66df96b
dest <g3candy@gmail.com>
2017-08-13 09:52:36 +0100

    rollback

--- ps.cpp
+++ ps.cpp
@@ -61,7 +61,6 @@ struct equity_future{
                         auto w = [task]()mutable{
                                 (*task)();
                         };
-                        pg.sequence_point();
                         pg.push(std::move(w));
                         return std::move(ret);
                 }
@@ -84,21 +83,28 @@ struct class_equity_future{
         result_t schedual_group(support::processor::process_group& pg, holdem_class_vector const& players){
                 std::cout << "schedual_group(" << players << "\n";
                 
-                std::vector< result_t > items;
+                std::vector< std::tuple< std::vector<int>, result_t > > items;
 
                 for( auto hv : players.get_hand_vectors()){
-                        auto fut = ef_.schedual_group(pg, hv);
-                        items.emplace_back(std::move(fut));
+                        auto p =  permutate_for_the_better(hv) ;
+                        auto& perm = std::get<0>(p);
+                        auto const& perm_players = std::get<1>(p);
+                        auto fut = ef_.schedual_group(pg, perm_players);
+                        items.emplace_back(perm, fut);
                 }
                 auto task = std::make_shared<std::packaged_task<std::shared_ptr<equity_breakdown>()> >(
                         [n_=players.size(),items_=std::move(items)](){
                         auto result = std::make_shared<equity_breakdown_matrix_aggregator>(n_);
-                        for( auto& item : items_ ){
-                                result->append( *item.get() );
+                        for( auto& t : items_ ){
+                                result->append(
+                                        *std::make_shared<equity_breakdown_permutation_view>(
+                                                std::get<1>(t).get(),
+                                                std::get<0>(t)));
                         }
                         return result;
                 });
                 result_t fut = task->get_future();
+                m_.emplace(players, fut);
                 pg.sequence_point();
                 pg.push([task]()mutable{ (*task)(); });
                 return fut;

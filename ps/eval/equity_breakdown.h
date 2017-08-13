e4eeddc2eee674f387c0674e1e1cdedea390fb63
dest <g3candy@gmail.com>
2017-08-13 19:19:01 +0100

    templated equity_breakdown

--- ps/eval/equity_breakdown.h
+++ ps/eval/equity_breakdown.h
@@ -14,25 +14,26 @@ namespace ps{
         // Need virtual interface, because sometimes want to
         // return a permutation of other results
 
-        struct equity_breakdown_player{
-                virtual double equity()const=0;
-                virtual size_t nwin(size_t idx)const=0;
-                virtual size_t sigma()const=0;
-                virtual size_t win()const{  return nwin(0); }
-                virtual size_t draw()const{ return nwin(1); }
-                virtual size_t lose()const=0;
-        };
 
-        struct equity_breakdown{
-                virtual size_t sigma()const=0;
+        template<class Primitive_Type>
+        struct basic_equity_breakdown{
+                using prim_t = Primitive_Type;
+                struct player_t{
+                        virtual double equity()const=0;
+                        virtual prim_t nwin(size_t idx)const=0;
+                        virtual prim_t sigma()const=0;
+                        virtual prim_t win()const{  return nwin(0); }
+                        virtual prim_t draw()const{ return nwin(1); }
+                        virtual prim_t lose()const=0;
+                };
+                virtual prim_t sigma()const=0;
                 virtual size_t n()const=0;
-                virtual equity_breakdown_player const& player(size_t idx)const=0;
-
+                virtual player_t const& player(size_t idx)const=0;
         };
-
-
-        struct equity_breakdown_permutation_view : equity_breakdown{
-                equity_breakdown_permutation_view(std::shared_ptr<equity_breakdown const> impl, std::vector<int> perm)
+                
+        template<class Primitive_Type>
+        struct basic_equity_breakdown_permutation_view : basic_equity_breakdown<Primitive_Type>{
+                basic_equity_breakdown_permutation_view(std::shared_ptr<basic_equity_breakdown<Primitive_Type> > const impl, std::vector<int> perm)
                         : impl_{impl}
                         , perm_{perm}
                 {
@@ -40,14 +41,19 @@ namespace ps{
                 }
                 size_t sigma()const override{ return impl_->sigma(); }
                 size_t n()    const override{ return impl_->n(); }
-                equity_breakdown_player const& player(size_t idx)const override{
+                typename basic_equity_breakdown<Primitive_Type>::player_t const& player(size_t idx)const override{
                         return impl_->player( perm_[idx] );
                 }
         private:
-                std::shared_ptr<equity_breakdown const> impl_;
+                std::shared_ptr<basic_equity_breakdown<Primitive_Type> const> impl_;
                 std::vector<int> perm_;
         }; 
-        
+
+
+        using equity_breakdown                  = basic_equity_breakdown<size_t>;
+        using equity_breakdown_player           = basic_equity_breakdown<size_t>::player_t;
+        using equity_breakdown_permutation_view = basic_equity_breakdown_permutation_view<size_t>;
+
 
                 
         std::ostream& operator<<(std::ostream& ostr, equity_breakdown const& self);

feb9845fe24b88ea0ea945b2c6820bdcd0d7fa50
Gerry Candy <gercan@hotmail.co.uk>
2018-10-07 13:58:44 +0100

    removed unused code

--- lib/eval/equity_evaluator_principal.cpp
+++
@@ -1,7 +0,0 @@
-#include "ps/eval/equity_evaluator_principal.h"
-
-namespace ps{
-namespace {
-        int ret = ( equity_evaluator_factory::register_<equity_evaulator_principal>("principal"), 0);
-}
-} // ps

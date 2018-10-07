beec7baa4e870c70e5e2bda752c1cd2949578316
Gerry Candy <gercan@hotmail.co.uk>
2018-10-07 13:54:47 +0100

    removed stupid equity cached stuff

--- lib/eval/equity_evaluator_cached.cpp
+++
@@ -1,8 +0,0 @@
-#include "ps/eval/equity_evaluator_cached.h"
-
-
-namespace ps{
-namespace{
-        int reg = ( equity_evaluator_factory::register_<equity_evaluator_cached>("cached"), 0);
-} // anon
-} // ps

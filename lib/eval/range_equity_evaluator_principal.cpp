9f3aa891040773dc1ae8a29cf1d7d359158a43c3
Gerry Candy <gercan@hotmail.co.uk>
2018-10-07 13:53:17 +0100

    removed pointless range_equity stuff

--- lib/eval/range_equity_evaluator_principal.cpp
+++
@@ -1,7 +0,0 @@
-#include "ps/eval/range_equity_evaluator_principal.h"
-
-namespace ps{
-namespace {
-        int reg = ( range_equity_evaluator_factory::register_<range_equity_evaluator_principal>("principal"), 0);
-} // anon
-} // ps

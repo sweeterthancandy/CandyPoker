3309f9c67e867278043746a13ef13d53011cb27f
Gerry Candy <gercan@hotmail.co.uk>
2018-10-14 14:40:44 +0100

    removed unused code

--- app/cmd_heads_up_solver.cpp
+++ app/cmd_heads_up_solver.cpp
@@ -216,11 +216,6 @@ namespace gt{
 
                         auto equity_vec = ( mask_.size() * ev - v_mask_ ) * ctx.eff() * p;
 
-                        #if 0
-                        for(size_t idx=0;idx!=mask_.size();++idx){
-                                out[mask_[idx]] += equity[idx] * p;
-                        }
-                        #endif
                         out += equity_vec;
                         out[vec.size()] += p;
                 }
@@ -294,52 +289,6 @@ namespace gt{
                 Eigen::VectorXd result{vec.size()+1};
                 result.fill(.0);
                 ctx.root()->evaluate(result, 1., ctx, vec,s);
-                #if 0
-                Eigen::VectorXd result{2};
-                result.fill(.0);
-                Eigen::VectorXd One{2};
-                One.fill(1.);
-
-                double f_ = ( 1.0 - s[0] );
-                double pf = s[0] * (1. - s[1]);
-                double pp = s[0] *       s[1];
-
-                #if 0
-                std::cout << "f_ => " << f_ << "\n"; // __CandyPrint__(cxx-print-scalar,f_)
-                std::cout << "pf => " << pf << "\n"; // __CandyPrint__(cxx-print-scalar,pf)
-                std::cout << "pp => " << pp << "\n"; // __CandyPrint__(cxx-print-scalar,pp)
-                #endif
-
-                result(0) -= ctx.sb() * f_;
-                result(1) += ctx.sb() * f_;
-                
-                result(0) += ctx.bb() * pf;
-                result(1) -= ctx.bb() * pf;
-
-                auto ev = cache.LookupVector(vec);
-
-                result += ( 2 * ev - One ) * ctx.eff() * pp;
-
-                do{
-                        Eigen::VectorXd other{vec.size()+1};
-                        other.fill(.0);
-                        static scratch::hu_game_tree tree(ctx);
-                        tree.evaluate(other, 1., ctx, cache, vec, s);
-
-
-                        double epsilon = 0.1;
-                        if( std::fabs( other(0) - result(0)) > epsilon || std::fabs(other(1) - result(1)) > epsilon ) {
-                                std::cout << "result => " << vector_to_string(result) << "\n"; // __CandyPrint__(cxx-print-scalar,result)
-                                std::cout << "other => " << vector_to_string(other) << "\n"; // __CandyPrint__(cxx-print-scalar,other)
-                                std::cout << "vector_to_string(s) => " << vector_to_string(s) << "\n"; // __CandyPrint__(cxx-print-scalar,vector_to_string(s))
-                        }
-
-
-                }while(0);
-
-
-
-                #endif
 
                 return result;
         }
@@ -361,10 +310,6 @@ namespace gt{
                         for(size_t idx=0;idx!=cv.size();++idx){
                                 s[idx] = S[idx][cv[idx]];
                         }
-                        #if 0
-                        s[0] = S[0][cv[0]];
-                        s[1] = S[1][cv[1]];
-                        #endif
                         auto meta_result = combination_value(ctx, cv, s);
                         result(cv[idx]) += p * meta_result[idx];
                 }
@@ -390,25 +335,14 @@ namespace gt{
         
         static Eigen::VectorXd fold_s = Eigen::VectorXd::Zero(169);
         static Eigen::VectorXd push_s = Eigen::VectorXd::Ones(169);
-        Eigen::VectorXd unilateral_sb_maximal_exploitable(gt_context const& ctx,
-                                                           std::vector<Eigen::VectorXd> const& S)
-        {
-                auto copy = S;
-                copy[0] = fold_s;
-                auto fold = unilateral_detail(ctx, 0, copy);
-                copy[0] = push_s;
-                auto push = unilateral_detail(ctx, 0, copy);
-                return choose_push_fold(push, fold);
-        }
-        Eigen::VectorXd unilateral_bb_maximal_exploitable(gt_context const& ctx,
-                                                           std::vector<Eigen::VectorXd> const& S)
+        Eigen::VectorXd unilateral_maximal_exploitable(gt_context const& ctx, size_t idx, std::vector<Eigen::VectorXd> const& S)
         {
 
                 auto copy = S;
-                copy[1] = fold_s;
-                auto fold = unilateral_detail(ctx, 1, copy);
-                copy[1] = push_s;
-                auto push = unilateral_detail(ctx, 1, copy);
+                copy[idx] = fold_s;
+                auto fold = unilateral_detail(ctx, idx, copy);
+                copy[idx] = push_s;
+                auto push = unilateral_detail(ctx, idx, copy);
                 return choose_push_fold(push, fold);
         }
 
@@ -417,12 +351,10 @@ namespace gt{
                                            std::vector<Eigen::VectorXd> const& state)
         {
                 double factor = 0.05;
-                auto bb_counter = unilateral_bb_maximal_exploitable(ctx,
-                                                                    state);
+                auto bb_counter = unilateral_maximal_exploitable(ctx,1, state);
                 auto tmp = state;
                 tmp[1] = bb_counter;
-                auto sb_counter = unilateral_sb_maximal_exploitable(ctx,
-                                                                    tmp);
+                auto sb_counter = unilateral_maximal_exploitable(ctx,0, tmp);
                 auto copy = state;
                 copy[0] *= ( 1 - factor );
                 copy[0] +=  factor * sb_counter;
@@ -431,78 +363,6 @@ namespace gt{
         }
 
 
-        #if NOT_DEFINED
-        std::vector<Eigen::VectorXd>
-        unilateral_chooser_step(gt_context const& ctx,
-                                std::vector<Eigen::VectorXd> const& state)
-        {
-                auto bb_counter = unilateral_bb_maximal_exploitable(ctx,
-                                                                    cache,
-                                                                    state);
-
-                auto head = unilateral_detail(ctx, cache, 0, state[0], bb_counter, false);
-                auto fold = unilateral_detail(ctx, cache, 0, fold_s, bb_counter, false);
-                auto push = unilateral_detail(ctx, cache, 0, push_s, bb_counter, false);
-
-                using Operator = std::tuple<size_t, double, double, double>;
-
-                std::vector<Operator> ops;
-
-                auto add_cand = [&](size_t idx, double cand, double cand_val){
-                        auto d = cand - head(idx);
-                        if( d > 0.0 ){
-                                ops.emplace_back(idx, cand, cand_val, d);
-                        }
-                };
-
-                for(size_t idx=0;idx!=169;++idx){
-                        add_cand(idx, fold(idx), 0.0);
-                        add_cand(idx, push(idx), 1.0);
-                }
-
-                std::sort(ops.begin(), ops.end(), [](auto const& l, auto const& r){
-                        return std::get<3>(l) > std::get<3>(r);
-                });
-
-                #if 0
-                enum{ CandidateSetSz = 3 };
-                if( ops.size() > CandidateSetSz ){
-                        ops.resize(CandidateSetSz);
-                }
-                std::vector<std::tuple<Eigen::VectorXd, double> > candidates;
-                for(auto const& t : ops){
-                        auto cand_strat = state[0];
-                        cand_strat[std::get<0>(t)] = std::get<2>(t);
-                        auto counter = unilateral_bb_maximal_exploitable(ctx,
-                                                                         cache,
-                                                                         cand_strat);
-                        candidates.emplace_back(cand_strat, unilateral_detail(ctx, cache, 0, cand_strat, counter).sum());
-
-                        std::cout << "holdem_class_decl::get(std::get<0>(t)) => " << holdem_class_decl::get(std::get<0>(t)) << "\n"; // __CandyPrint__(cxx-print-scalar,holdem_class_decl::get(std::get<0>(t)))
-                        std::cout << "std::get<1>(candidates.back()) => " << std::get<1>(candidates.back()) << "\n"; // __CandyPrint__(cxx-print-scalar,std::get<1>(candidates.back()))
-                }
-                std::sort(candidates.begin(), candidates.end(), [](auto const& r, auto const& l){
-                        return std::get<1>(r) < std::get<1>(l);
-                });
-                
-                auto copy = state;
-                if( candidates.size()){
-                        copy[0] = std::get<0>(candidates.back());
-                }
-                copy[1] = bb_counter;
-                #endif
-
-                auto copy = state;
-                ops.resize(ops.size()/2);
-                copy[1] = bb_counter;
-                for(auto const& t : ops){
-                        auto cand_strat = state[0];
-                        copy[0][std::get<0>(t)] = std::get<2>(t);
-                }
-                return copy;
-
-        }
-        #endif // NOT_DEFINED
 
 
         
@@ -516,12 +376,10 @@ namespace gt{
                 virtual std::vector<Eigen::VectorXd> step(gt_context const& ctx,
                                                           std::vector<Eigen::VectorXd> const& state)override
                 {
-                        auto bb_counter = unilateral_bb_maximal_exploitable(ctx,
-                                                                            state);
+                        auto bb_counter = unilateral_maximal_exploitable(ctx, 1, state);
                         auto tmp = state;
                         tmp[1] = bb_counter;
-                        auto sb_counter = unilateral_sb_maximal_exploitable(ctx,
-                                                                            tmp);
+                        auto sb_counter = unilateral_maximal_exploitable(ctx, 0, tmp);
                         auto copy = state;
                         copy[0] *= ( 1 - factor_ );
                         copy[0] +=  factor_ * sb_counter;
@@ -623,68 +481,6 @@ namespace gt{
                 stoppage_condition_t stop_cond_;
                 size_t max_steps_{DefaultMaxIter};
         };
-        
-
-        #if 0
-        std::vector<Eigen::VectorXd> solve(gt_context const& ctx,
-                                           class_cache const& cache,
-                                           std::shared_ptr<solver> solver_impl,
-                                           std::vector<Eigen::VectorXd> const& state0)
-        {
-
-                std::vector<Eigen::VectorXd> state = state0;
-                #if 0
-                state.push_back(Eigen::VectorXd::Zero(169));
-                state.push_back(Eigen::VectorXd::Zero(169));
-                
-                std::shared_ptr<solver> solver_impl( new maximal_exploitable_solver);
-                #endif
-
-
-                double epsilon = 0.2;
-
-                enum{ MaxIter = 400 };
-                for(size_t idx=0;idx<MaxIter;++idx){
-
-                        auto next = solver_impl->step(ctx, cache, state);
-                        auto d = next[0] - state[0];
-                        auto norm = d.lpNorm<1>();
-
-                        #if 0
-                        std::cout << "norm => " << norm << "\n"; // __CandyPrint__(cxx-print-scalar,norm)
-                        #endif
-
-                        if( norm < epsilon ){
-                                state[0] = clamp(state[0]);
-                                state[1] = clamp(state[1]);
-                                return state;
-                        }
-
-                        #if 0
-                        if( memory.count( next ) != 0 ){
-                                BOOST_LOG_TRIVIAL(warning) << "loop";
-                        }
-                        memory.insert(next);
-                        #endif
-
-                        state = next;
-
-                        #if 0
-                        std::cout << "S_0\n";
-                        pretty_print_strat(state[0], 1);
-                        std::cout << "S_1\n";
-                        pretty_print_strat(state[1], 1);
-                        #endif
-                }
-
-                std::vector<Eigen::VectorXd> result;
-                result.push_back(Eigen::VectorXd::Zero(169));
-                result.push_back(Eigen::VectorXd::Zero(169));
-
-                BOOST_LOG_TRIVIAL(warning) << "Failed to converge solve ctx = " << ctx;
-                return result;
-        }
-        #endif
 
 
 
@@ -725,7 +521,7 @@ struct HeadUpSolverCmd : Command{
                         }));
                 };
                 #if 0
-                for(double eff = 1.0;eff <= 30.0;eff+=.1){
+                for(double eff = 10.0;eff <= 20.0;eff+=1){
                         enque(eff);
                 }
                 #else
@@ -757,5 +553,90 @@ private:
         std::vector<std::string> const& args_;
 };
 static TrivialCommandDecl<HeadUpSolverCmd> HeadsUpSolverCmdDecl{"heads-up-solver"};
+        
+
+
+
+
+
+
+
+
+
+
+
+
+#if NOT_DEFINED
+        std::vector<Eigen::VectorXd>
+        unilateral_chooser_step(gt_context const& ctx,
+                                std::vector<Eigen::VectorXd> const& state)
+        {
+                auto bb_counter = unilateral_bb_maximal_exploitable(ctx,
+                                                                    cache,
+                                                                    state);
+
+                auto head = unilateral_detail(ctx, cache, 0, state[0], bb_counter, false);
+                auto fold = unilateral_detail(ctx, cache, 0, fold_s, bb_counter, false);
+                auto push = unilateral_detail(ctx, cache, 0, push_s, bb_counter, false);
+
+                using Operator = std::tuple<size_t, double, double, double>;
+
+                std::vector<Operator> ops;
+
+                auto add_cand = [&](size_t idx, double cand, double cand_val){
+                        auto d = cand - head(idx);
+                        if( d > 0.0 ){
+                                ops.emplace_back(idx, cand, cand_val, d);
+                        }
+                };
+
+                for(size_t idx=0;idx!=169;++idx){
+                        add_cand(idx, fold(idx), 0.0);
+                        add_cand(idx, push(idx), 1.0);
+                }
+
+                std::sort(ops.begin(), ops.end(), [](auto const& l, auto const& r){
+                        return std::get<3>(l) > std::get<3>(r);
+                });
+
+                #if 0
+                enum{ CandidateSetSz = 3 };
+                if( ops.size() > CandidateSetSz ){
+                        ops.resize(CandidateSetSz);
+                }
+                std::vector<std::tuple<Eigen::VectorXd, double> > candidates;
+                for(auto const& t : ops){
+                        auto cand_strat = state[0];
+                        cand_strat[std::get<0>(t)] = std::get<2>(t);
+                        auto counter = unilateral_bb_maximal_exploitable(ctx,
+                                                                         cache,
+                                                                         cand_strat);
+                        candidates.emplace_back(cand_strat, unilateral_detail(ctx, cache, 0, cand_strat, counter).sum());
+
+                        std::cout << "holdem_class_decl::get(std::get<0>(t)) => " << holdem_class_decl::get(std::get<0>(t)) << "\n"; // __CandyPrint__(cxx-print-scalar,holdem_class_decl::get(std::get<0>(t)))
+                        std::cout << "std::get<1>(candidates.back()) => " << std::get<1>(candidates.back()) << "\n"; // __CandyPrint__(cxx-print-scalar,std::get<1>(candidates.back()))
+                }
+                std::sort(candidates.begin(), candidates.end(), [](auto const& r, auto const& l){
+                        return std::get<1>(r) < std::get<1>(l);
+                });
+                
+                auto copy = state;
+                if( candidates.size()){
+                        copy[0] = std::get<0>(candidates.back());
+                }
+                copy[1] = bb_counter;
+                #endif
+
+                auto copy = state;
+                ops.resize(ops.size()/2);
+                copy[1] = bb_counter;
+                for(auto const& t : ops){
+                        auto cand_strat = state[0];
+                        copy[0][std::get<0>(t)] = std::get<2>(t);
+                }
+                return copy;
+
+        }
+        #endif // NOT_DEFINED
 
 } // end namespace ps

2a6bf3609514675010232f6cd23ec3a44b4dd23d
Gerry Candy <gercan@hotmail.co.uk>
2019-02-17 13:11:12 +0000

    added intrinisc version ignoring null weights

--- lib/eval/dispatch_three_avx2.cpp
+++ lib/eval/dispatch_three_avx2.cpp
@@ -224,4 +224,142 @@ namespace ps{
                 virtual size_t precedence()const override{ return 101; }
         };
         static register_disptach_table<dispatch_three_player_avx2> reg_dispatch_three_player_avx2;
+        
+        
+        
+        
+        
+        
+        struct scheduler_intrinsic_three_avx2_opt{
+                enum{ CheckUnion = true };
+                template<class SubPtrType>
+                struct bind{
+                        explicit bind(size_t batch_size, std::vector<SubPtrType>& subs)
+                                :subs_{subs}
+                        {
+                                evals_.resize(batch_size);
+
+                        }
+                        void put(size_t index, ranking_t rank)noexcept{
+                                evals_[index] = rank;
+                        }
+                        void end_eval(mask_set const* ms, size_t single_mask)noexcept{
+
+                                //#define DBG(REG, X) do{ std::cout << #REG "[" << (X) << "]=" << std::bitset<16>(_mm256_extract_epi16(REG,X)).to_string() << "\n"; }while(0)
+                                #define DBG(REG, X) do{}while(0)
+
+                                __m256i m0 = _mm256_set1_epi16(0b001);
+                                __m256i m1 = _mm256_set1_epi16(0b010);
+                                __m256i m2 = _mm256_set1_epi16(0b100);
+
+                                #define FOR_EACH(X) \
+                                do {\
+                                        X(0);\
+                                        X(1);\
+                                        X(2);\
+                                        X(3);\
+                                        X(4);\
+                                        X(5);\
+                                        X(6);\
+                                        X(7);\
+                                        X(8);\
+                                        X(9);\
+                                        X(10);\
+                                        X(11);\
+                                        X(12);\
+                                        X(13);\
+                                        X(14);\
+                                        X(15);\
+                                }while(0)
+
+
+                                size_t head=0; 
+                                size_t tail=0;
+
+                                std::array<decltype(std::declval<SubPtrType>().get()), 16> batch;
+                                std::array<size_t, 16> batch_weight;
+
+                                for(; head != subs_.size();tail=head){
+                                        __m256i v0 = _mm256_setzero_si256();
+                                        __m256i v1 = _mm256_setzero_si256();
+                                        __m256i v2 = _mm256_setzero_si256();
+
+                                        #define SCATTER(N)                                                                 \
+                                                do{                                                                        \
+                                                        for(;head!=subs_.size() && count != 16;++head){\
+                                                                size_t weight = generic_weight_policy{} \
+                                                                        .calculate( subs_[head]->hand_mask(), ms, single_mask);        \
+                                                                if( weight == 0 ) continue; \
+                                                                batch_weight[N] = weight;\
+                                                                batch[N] = subs_[head].get();\
+                                                                batch[N]->template prepare_intrinsic_3<N>(evals_,  v0, v1, v2);\
+                                                                ++count;\
+                                                                ++head;\
+                                                                break;\
+                                                        }\
+                                                }while(0)
+
+                                        size_t count = 0;
+                                        FOR_EACH(SCATTER);
+                                        // no edge cases
+                                        if( count != 16 )
+                                                break;
+
+
+                                        __m256i r_min = _mm256_min_epi16(v0, _mm256_min_epi16(v1, v2));
+                                        __m256i eq0 =_mm256_cmpeq_epi16(r_min, v0);
+                                        __m256i eq1 =_mm256_cmpeq_epi16(r_min, v1);
+                                        __m256i eq2 =_mm256_cmpeq_epi16(r_min, v2);
+                                        __m256i a0 = _mm256_and_si256(eq0, m0);
+                                        __m256i a1 = _mm256_and_si256(eq1, m1);
+                                        __m256i a2 = _mm256_and_si256(eq2, m2);
+                                        __m256i mask = _mm256_or_si256(a0, _mm256_or_si256(a1, a2));
+
+                                        #define GATHER(N)                                                               \
+                                                do{                                                                     \
+                                                        batch[N]->template accept_intrinsic_3< N>(batch_weight[N], evals_, mask);\
+                                                }while(0)
+
+                                        FOR_EACH(GATHER);
+
+                                        
+                                }
+                                for(;tail!=subs_.size();++tail){
+                                        size_t weight = generic_weight_policy{}
+                                                .calculate(subs_[tail]->hand_mask(), ms, single_mask);
+                                        subs_[tail]->accept_weight(weight, evals_);
+                                }
+                        }
+                        void regroup()noexcept{
+                                // nop
+                        }
+                private:
+                        std::vector<ranking_t> evals_;
+                        std::vector<SubPtrType>& subs_;
+                        mask_set const* ms_;
+                        size_t out_{0};
+                };
+        };
+        
+        struct dispatch_three_player_avx2_opt : dispatch_table{
+                using transform_type =
+                        optimized_transform<
+                        sub_eval_three_intrinsic_avx2,
+                        scheduler_intrinsic_three_avx2_opt,
+                        basic_sub_eval_factory,
+                        rank_hash_eval>;
+
+
+                virtual bool match(dispatch_context const& dispatch_ctx)const override{
+                        return dispatch_ctx.homo_num_players &&  dispatch_ctx.homo_num_players.get() == 3;
+                }
+                virtual std::shared_ptr<optimized_transform_base> make()const override{
+                        return std::make_shared<transform_type>();
+                }
+                virtual std::string name()const override{
+                        return "three-player-avx2-opt";
+                }
+                virtual size_t precedence()const override{ return 102; }
+        };
+        static register_disptach_table<dispatch_three_player_avx2_opt> reg_dispatch_three_player_avx2_opt;
 } // end namespace ps

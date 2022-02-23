56a48f46525df1e9e18893959423024c44e9022f
Gerry Candy <g3candy@gmail.com>
2022-02-23 22:10:23 +0000

    added inital unit test work

--- gtest/hello_test.cc
+++ gtest/hello_test.cc
@@ -1,9 +1,18 @@
 #include <gtest/gtest.h>
 
+#include "ps/base/frontend.h"
+
+
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+        auto range = ps::frontend::parse("AA");
+        auto primitive_range = range.to_primitive_range();
+        auto holdem_vector = range.to_holdem_vector();
+        auto class_vec = range.to_class_vector();
+
+        std::cout << "range => " << range << "\n"; // __CandyPrint__(cxx-print-scalar,range)
+        std::cout << "primitive_range => " << primitive_range << "\n"; // __CandyPrint__(cxx-print-scalar,primitive_range)
+        std::cout << "holdem_vector => " << holdem_vector << "\n"; // __CandyPrint__(cxx-print-scalar,holdem_vector)
+        std::cout << "class_vec => " << class_vec << "\n"; // __CandyPrint__(cxx-print-scalar,class_vec)
+
 }

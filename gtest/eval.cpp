988c55bd8eb605797849cce6a758c04134769baa
Gerry Candy <g3candy@gmail.com>
2022-02-26 12:25:53 +0000

    added another reval example

--- gtest/eval.cpp
+++ gtest/eval.cpp
@@ -58,3 +58,23 @@ TEST(Eval, AAo_vs_22_vs_87s) {
         EXPECT_NEAR(P1.EquityAsDouble(), 0.2619, 0.0005);
         EXPECT_NEAR(P2.EquityAsDouble(), 0.3562, 0.0005);
 }
+
+
+
+TEST(Eval, TwoPlayerRange) {
+        std::vector<std::string> player_ranges{ "AKs,AQs,AKo", "AA,KK,QQ" };
+        auto result = evaluate(player_ranges);
+
+        const auto P0 = result.player_view(0);
+        const auto P1 = result.player_view(1);
+
+        EXPECT_EQ( P0.Wins(), 125'645'556 );
+        EXPECT_EQ( P0.AnyDraws(), 3'148'680 );
+        
+        EXPECT_EQ( P1.Wins(), 282'158'724 );
+        EXPECT_EQ( P1.AnyDraws(), 3'148'680 );
+
+        EXPECT_EQ( P0.EquityAsRational(), rational_ty(588981,1902560));
+        EXPECT_EQ( P1.EquityAsRational(), rational_ty(1313579,1902560));
+
+}

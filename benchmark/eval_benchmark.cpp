3d61c352ddaf02ca1b41091767414acdaf5cade3
Gerry Candy <g3candy@gmail.com>
2022-02-27 12:44:19 +0000

    added more benchmarks

--- benchmark/eval_benchmark.cpp
+++ benchmark/eval_benchmark.cpp
@@ -12,25 +12,27 @@
 using namespace ps;
 using namespace ps::interface_;
 
+inline auto do_frontend_parse(std::string const& s)
+{
+    return frontend::parse(s);
+}
 
 
 static void BM_FrontPlayerRangeSimple(benchmark::State& state) {
 
     for (auto _ : state)
     {
-        frontend::parse("AA");
+        do_frontend_parse("AA");
     }
 }
-BENCHMARK(BM_FrontPlayerRangeSimple);
-
-
 static void BM_FrontPlayerRangeComplex(benchmark::State& state) {
 
     for (auto _ : state)
     {
-        frontend::parse("TT+,AKo+,AQs+");
+        do_frontend_parse("45s+,68s+,22+,ATo+,AJs+");
     }
 }
+BENCHMARK(BM_FrontPlayerRangeSimple);
 BENCHMARK(BM_FrontPlayerRangeComplex);
 
 
@@ -39,36 +41,36 @@ static void BM_FrontTwoPlayerTreeSimple(benchmark::State& state) {
     
     for (auto _ : state)
     {
-        auto p0 = frontend::parse("AA");
-        auto p1 = frontend::parse("KK");
+        auto p0 = do_frontend_parse("AA");
+        auto p1 = do_frontend_parse("KK");
         tree_range root({ p0,p1 });
     }
 }
-BENCHMARK(BM_FrontTwoPlayerTreeSimple);
 
 static void BM_FrontThreePlayerTreeSimple(benchmark::State& state) {
 
     for (auto _ : state)
     {
-        auto p0 = frontend::parse("AA");
-        auto p1 = frontend::parse("KK");
-        auto p2 = frontend::parse("QQ");
+        auto p0 = do_frontend_parse("AA");
+        auto p1 = do_frontend_parse("KK");
+        auto p2 = do_frontend_parse("QQ");
         tree_range root({ p0,p1,p2 });
     }
 }
-BENCHMARK(BM_FrontThreePlayerTreeSimple);
 
 static void BM_FrontFourPlayerTreeSimple(benchmark::State& state) {
 
     for (auto _ : state)
     {
-        auto p0 = frontend::parse("AA");
-        auto p1 = frontend::parse("KK");
-        auto p2 = frontend::parse("QQ");
-        auto p3 = frontend::parse("JJ");
+        auto p0 = do_frontend_parse("AA");
+        auto p1 = do_frontend_parse("KK");
+        auto p2 = do_frontend_parse("QQ");
+        auto p3 = do_frontend_parse("JJ");
         tree_range root({ p0,p1,p2,p3 });
     }
 }
+BENCHMARK(BM_FrontTwoPlayerTreeSimple);
+BENCHMARK(BM_FrontThreePlayerTreeSimple);
 BENCHMARK(BM_FrontFourPlayerTreeSimple);
 
 
@@ -81,8 +83,8 @@ static void BM_TwoPlayerInstructionBuilderSimple(benchmark::State& state) {
 
     for (auto _ : state)
     {
-        auto p0 = frontend::parse("AA");
-        auto p1 = frontend::parse("KK");
+        auto p0 = do_frontend_parse("AA");
+        auto p1 = do_frontend_parse("KK");
         frontend_to_instruction_list("dummy", { p0, p1 });
     }
 }
@@ -90,9 +92,9 @@ static void BM_ThreePlayerInstructionBuilderSimple(benchmark::State& state) {
 
     for (auto _ : state)
     {
-        auto p0 = frontend::parse("AA");
-        auto p1 = frontend::parse("KK");
-        auto p2 = frontend::parse("QQ");
+        auto p0 = do_frontend_parse("AA");
+        auto p1 = do_frontend_parse("KK");
+        auto p2 = do_frontend_parse("QQ");
         frontend_to_instruction_list("dummy", { p0, p1, p2 });
     }
 }
@@ -100,10 +102,10 @@ static void BM_FourPlayerInstructionBuilderSimple(benchmark::State& state) {
 
     for (auto _ : state)
     {
-        auto p0 = frontend::parse("AA");
-        auto p1 = frontend::parse("KK");
-        auto p2 = frontend::parse("QQ");
-        auto p3 = frontend::parse("JJ");
+        auto p0 = do_frontend_parse("AA");
+        auto p1 = do_frontend_parse("KK");
+        auto p2 = do_frontend_parse("QQ");
+        auto p3 = do_frontend_parse("JJ");
         frontend_to_instruction_list("dummy", { p0, p1, p2, p3 });
     }
 }

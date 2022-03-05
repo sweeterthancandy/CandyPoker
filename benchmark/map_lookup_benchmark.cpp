07cb6a840836e4672eb90cdb67cc6c57502887c8
Gerry Candy <g3candy@gmail.com>
2022-03-05 11:49:37 +0000

    added map lookup benchmark

--- benchmark/map_lookup_benchmark.cpp
+++ benchmark/map_lookup_benchmark.cpp
@@ -16,69 +16,107 @@ static void sleep_test(benchmark::State& state)
 BENCHMARK(sleep_test);
 #endif
 
+using value_ty = size_t;
 
-static void map_lookup(benchmark::State& state, size_t max_domain_value)
+struct MapLookupAux
 {
-        constexpr size_t lookup_size = 1'000'00;
-
-        using value_ty = size_t;
-
-        constexpr value_ty max_range_value = 123546789;
-
         static std::vector<value_ty> lookup_vec;
         static std::vector<value_ty> values_to_lookup;
 
-        constexpr size_t vec_size = 1ull << 28;
-
-        if (lookup_vec.empty())
+        explicit MapLookupAux(size_t max_domain_value)
         {
-                lookup_vec.resize(vec_size);
-                values_to_lookup.resize(lookup_size);
-        }
+                constexpr size_t lookup_size = 1'000'00;
+                constexpr value_ty max_range_value = 123546789;
 
-        std::random_device seeder;
-        std::mt19937 engine(seeder());
+        
 
-        std::uniform_int_distribution<value_ty> domain_dist(0, static_cast<value_ty>(max_domain_value));
-        std::uniform_int_distribution<value_ty> range_dist(0, max_range_value);
+                constexpr size_t vec_size = 1ull << 28;
+                if (lookup_vec.empty())
+                {
+                        lookup_vec.resize(vec_size);
+                        values_to_lookup.resize(lookup_size);
+                }
+
+                std::random_device seeder;
+                std::mt19937 engine(seeder());
+
+                std::uniform_int_distribution<value_ty> domain_dist(0, static_cast<value_ty>(max_domain_value));
+                std::uniform_int_distribution<value_ty> range_dist(0, max_range_value);
 
         
         
                         
-        // this shouldn't really matter
-        constexpr size_t num_non_zero = 10000;
-        for(size_t idx=0;idx!=num_non_zero;++idx)
-        {
-                lookup_vec[domain_dist(engine)] = range_dist(engine);
-        }
+                // this shouldn't really matter
+                constexpr size_t num_non_zero = 10000;
+                for(size_t idx=0;idx!=num_non_zero;++idx)
+                {
+                        lookup_vec[domain_dist(engine)] = range_dist(engine);
+                }
                         
-        for(size_t idx=0;idx!=lookup_size;++idx)
-        {
-                values_to_lookup[idx] = domain_dist(engine);
+                for(size_t idx=0;idx!=lookup_size;++idx)
+                {
+                        values_to_lookup[idx] = domain_dist(engine);
+                }
         }
+};
 
+std::vector<value_ty> MapLookupAux::lookup_vec;
+std::vector<value_ty> MapLookupAux::values_to_lookup;
 
+static void MapLookupUnconditional(benchmark::State& state)
+{
+        
+        const size_t max_domain_value = state.range(0);
+        MapLookupAux aux(max_domain_value);
 
         for (auto _ : state)
         {  
-
-                for (auto key : values_to_lookup)
+                for (auto key : aux.values_to_lookup)
                 {
-                        const auto value = lookup_vec[key];
+                        const auto value = aux.lookup_vec[key];
                         benchmark::DoNotOptimize(value);
                 }
-                
         }  
 }
+BENCHMARK(MapLookupUnconditional)->RangeMultiplier(2)->Range(1ull << 8, 1ull << 28);
 
-BENCHMARK_CAPTURE(map_lookup, 4, 1ull << 4);
-BENCHMARK_CAPTURE(map_lookup, 6, 1ull << 6);
-BENCHMARK_CAPTURE(map_lookup, 8, 1ull << 8);
-BENCHMARK_CAPTURE(map_lookup, 16, 1ull << 16);
-BENCHMARK_CAPTURE(map_lookup, 20, 1ull << 20);
-BENCHMARK_CAPTURE(map_lookup, 22, 1ull << 22);
-BENCHMARK_CAPTURE(map_lookup, 24, 1ull << 24);
-BENCHMARK_CAPTURE(map_lookup, 26, 1ull << 26);
-BENCHMARK_CAPTURE(map_lookup, 28, 1ull << 28);
+
+static void MapLookupConditionalUnlikelyLookup(benchmark::State& state)
+{
+        
+        const size_t max_domain_value = state.range(0);
+        MapLookupAux aux(max_domain_value);
+
+        for (auto _ : state)
+        {  
+                for (auto key : aux.values_to_lookup)
+                {
+                        if( ( key % 27 ) != 0 )
+                                continue;
+                        const auto value = aux.lookup_vec[key];
+                        benchmark::DoNotOptimize(value);
+                }
+        }  
+}
+BENCHMARK(MapLookupConditionalUnlikelyLookup)->RangeMultiplier(2)->Range(1ull << 8, 1ull << 28);
+
+static void MapLookupConditionalLikelyLookup(benchmark::State& state)
+{
+        
+        const size_t max_domain_value = state.range(0);
+        MapLookupAux aux(max_domain_value);
+
+        for (auto _ : state)
+        {  
+                for (auto key : aux.values_to_lookup)
+                {
+                        if( ( key % 27 ) == 0 )
+                                continue;
+                        const auto value = aux.lookup_vec[key];
+                        benchmark::DoNotOptimize(value);
+                }
+        }  
+}
+BENCHMARK(MapLookupConditionalLikelyLookup)->RangeMultiplier(2)->Range(1ull << 8, 1ull << 28);
 
 BENCHMARK_MAIN();
